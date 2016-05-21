02dca5d2f41027f8c03d3fe71ff0519956fdb087
juel <junaidelahi@hotmail.com>
2016-05-21 18:11:03 +0200

    aliases.zsh refactored.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,28 +1,29 @@
 
+#--------------------------------------------------------------------
+# Global aliases
+#--------------------------------------------------------------------
+
 # With zsh you can have global (-g) aliases (for common patterns) and suffix (-s) aliases
 # Examples:
 #	alias -s pdf=xpdf       ; # executing 'foo.pdf' translates to 'xpdf foo.pdf'
 #	alias -g TT='| tail -n' ; # can be used as 'history TT 10'
 
-alias sz='source ${ZDOTDIR}/.zshrc'
+alias -g G="| grep"
+alias -g L="| less"
 
-# {{{ General ...
+#--------------------------------------------------------------------
+# Directory movement
+#--------------------------------------------------------------------
+
 alias -- .='pwd'
 alias -- ..='cd ..'
 alias -- /='cd /'
 alias -- -='cd -'
 
-alias cp='cp -v -i'
-alias mv='mv -v -i'
-alias rm='nocorrect rm -v -i'
-alias h='history'
-alias chmodx='chmod +x'
-# }}}
+#--------------------------------------------------------------------
+# Environment modules
+#--------------------------------------------------------------------
 
-# Sublime Text
-alias st='subl'
-
-# {{{ Environment modules ...
 alias modld='module load'
 alias moduld='module unload'
 alias modrld='module reload'
@@ -30,9 +31,17 @@ alias modall='module avail'
 alias modls='module list'
 alias modpg='module purge'
 alias modswp='modpg; modld'
-# }}}
 
-# {{{ Globals ...
-alias -g G="| grep"
-alias -g L="| less"
-# }}}
+#--------------------------------------------------------------------
+# Oddball stuff
+#--------------------------------------------------------------------
+
+alias cp='cp -v -i'
+alias mv='mv -v -i'
+alias rm='nocorrect rm -v -i'
+alias chmodx='chmod +x'
+
+alias h='history'
+alias st='subl'
+
+alias sz='source ${ZDOTDIR}/.zshrc'
